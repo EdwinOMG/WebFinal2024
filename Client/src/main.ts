2f5a26c1b7580e6cafe81af0a81cc024298cc644
EdwinOMG <150313733+EdwinOMG@users.noreply.github.com>
2024-11-04 13:03:22 -0500

    added some imports

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'bulma/css/bulma.css'
 
 const app = createApp(App)
 
