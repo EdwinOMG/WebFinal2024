b0ab43ee26f3b8ce30859567de129504606ba7be
EdwinOMG <150313733+EdwinOMG@users.noreply.github.com>
2024-11-04 13:01:23 -0500

    deleted unnecessary imports

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,24 +1,8 @@
-import 'bulma/css/bulma.css'
-import '@fortawesome/fontawesome-free'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faPersonWalking } from '@fortawesome/free-solid-svg-icons/faPersonWalking'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import { faLock } from '@fortawesome/free-solid-svg-icons/faLock'
-import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck'
-import { faUser } from '@fortawesome/free-solid-svg-icons/faUser'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope'
-
-library.add(faPersonWalking, faMagnifyingGlass, faLock, faCheck, faEnvelope, faUser)
-
 const app = createApp(App)
-
-app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
