9f4f8ad24f9b36d6b0bc9d7747cec5f736069f22
EdwinOMG <150313733+EdwinOMG@users.noreply.github.com>
2024-11-14 14:34:36 -0500

    server setup, controller setup, and created express server

---
+++ Server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const userController = require("./controllers/users");
+
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.static(__dirname + "/dist"));
+
+// Controllers
+app
+  .get("/", (req, res, next) => {
+    res.send("Hello World");
+  })
+  .get("/about", (req, res, next) => {
+    res.send("About Us");
+  })
+  .use("/api/v1/users", userController)
+  /* add other controllers here */
+  .get("*", (req, res, next) => {
+    res.sendFile(__dirname + "/dist/index.html");
+  });
+
+console.log("Step #1");
+app.listen(PORT, (err, data) => {
+  console.log("Step #2");
+  console.log("Server is running at http://localhost:" + PORT);
+});
+console.log("Step #3");
+
+/*  Four types of async methods
+    1. Node Style Callbacks
+    2. Pipelines
+    3. Promises
+    4. Async/Await
+*/
