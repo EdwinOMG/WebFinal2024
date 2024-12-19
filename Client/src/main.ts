c3b1893c719edc5b174a1049563f84d66bdb2c5f
EdwinOMG <150313733+EdwinOMG@users.noreply.github.com>
2024-12-19 14:30:02 -0500

    added imports and autocomplete

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@fortawesome/fontawesome-free/css/all.css'
+import '@oruga-ui/theme-oruga/dist/oruga.css'
 import 'bulma/css/bulma.css'
+import { Autocomplete } from '@oruga-ui/oruga-next'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router).use(Autocomplete)
 
 app.mount('#app')
